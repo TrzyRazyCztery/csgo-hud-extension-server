83b5fe1671fd2a7d520e744b7649c13ebf9d758e
TrzyRazyCztery <3x4to9@gmail.com>
2019-04-16 17:28:44 +0200

    code splitted to controllers

--- server.js
+++ server.js
@@ -1,54 +1,28 @@
-const fs = require('fs')
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
 const app = express();
-const debug = require('debug')('http')
-const port = 3030;
-//app.use(bodyParser.json());
+const port = 3037;
+const dataSource = {
+  "76561197993506684" : 
+    {steamid: "76561197993506684", personaname: "nerf xyp9x", avatar: "https://steamcdn-a.akamaihd.net/steamcommunity/pub…s/44/448818a1b37b199c0579a667e495ed5c40e675c9.jpg"}
+  , token: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InN0ZWFtSWQiOiI3NjU2MTE5Nzk5MzUwNjY4NCJ9LCJpYXQiOjE1NTQ5MTI3OTksImV4cCI6MTU1NTUxNzU5OX0.OZqEhKuRZbsC_baSP1F07g4CtMSJQx5V6NkEolByEufSbo42mrRp-G3tSj7SyQKkhBwu8BdCUc3o3EjDcBCPtA"
+};
+const liveData = require("./liveData");
+const auth = require("./auth");
 
-var responseSSE = {write: ()=> {}};
-app.get('/', (req, res) => res.send('Hello World'))
-app.get('/events', (req, res) =>  {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    console.log(req.headers)
-    res.writeHead(200,{
-        Connection:"keep-alive",
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache"})
-    responseSSE = res;
-})
+app.all("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
+app.use("/livedata", liveData.app(dataSource))
 
-
-app.post('/livedata', (req, res) => {
-
-    var body = '';
-    
-    req.on('data', function (data) {
-        body += data
-    });
-    req.on('end', function () {
-        responseSSE.write(`data:${JSON.stringify(body)}`);
-        responseSSE.write("\n\n")
-        res.end( '' );
-    });
-    
+app.use((req, res, next) => {
+  next();
+});
 
 
-})
+//app.use("/livedata", liveData).app;
+app.use("/auth", auth.app(dataSource));
 
-
-
-// app.post('/livedata', (req, res) => {
-//     res.writeHead(200, {'Content-Type': 'application/json'});
-
-
-//     console.log(req.body.player.weapons.weapon_2.ammo_clip)
-//     responseSSE.write(`data: ${req.body}`);
-//     responseSSE.write("\n\n")
-
-
-// })
-app.listen(port, () => console.log(`app listen on ${port}`))
-
+app.listen(port, () => console.log(`app listen on ${port}`));
